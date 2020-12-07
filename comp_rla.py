e55cbedb6a2ef06bba8a39ff034edd5eeddb83cc
jbrasch <jbrasch@umich.edu>
2020-12-07 01:32:47 -0500

    sim rla fix:

--- comp_rla.py
+++ comp_rla.py
@@ -76,39 +76,22 @@ def simultaneous_rla(election1, election2, risk_limit=0.5, inflator=1.1, toleran
     if margin >= 1:
         return [margin, total_votes, initial_sample, expected]
 
-    # # Kaplan-Markov P-Value
-    # def km_pval(ballots_drawn):
-    #     n_1 = ballots_drawn * ONE_ERROR
-    #     n_2 = ballots_drawn * TWO_ERROR
-    #     a = pow(1 - margin / 2 / tolerance, ballots_drawn)
-    #     b = pow(1 - 1 / 2 / inflator, n_1)
-    #     c = pow(1 - 1 / inflator, n_2)
-    #     return 1 if b == 0 or c == 0 else a / b / c
+    # Kaplan-Markov P-Value
+    def km_pval(ballots_drawn):
+        n_1 = ballots_drawn * ONE_ERROR
+        n_2 = ballots_drawn * TWO_ERROR
+        a = pow(1 - margin / 2 / inflator, ballots_drawn)
+        b = pow(1 - 1 / 2 / inflator, n_1)
+        c = pow(1 - 1 / inflator, n_2)
+        return 1 if b == 0 or c == 0 else a / b / c
 
-    # # if tolerance * margin < ONE_ERROR + TWO_ERROR:
-    # pkm = km_pval(expected)
-    # while pkm > risk_limit and expected < total_votes:
-    #     expected += 1
-    #     pkm = km_pval(expected)
-    #     if pkm == 1:
-    #         expected = total_votes
-    #         break
-
-    def ballot_bound(ballots_drawn):
-        o_1 = ballots_drawn * ONE_ERROR
-        o_2 = ballots_drawn * TWO_ERROR * 5
-        u_1 = ballots_drawn * ONE_ERROR * 0.6
-        u_2 = ballots_drawn * TWO_ERROR * 4.4
-        return (mult + 1.4 * (o_1 + o_2 - u_1 - u_2)) / margin
-
-    draw_lower = ballot_bound(expected)
-    while expected < draw_lower and draw_lower < total_votes:
-        expected += 1
-        draw_lower = ballot_bound(expected)
-        if draw_lower - expected >= total_votes - expected:
-            draw_lower = total_votes
-
-    if draw_lower >= total_votes:
-        expected = total_votes
+    if tolerance * margin < (ONE_ERROR + TWO_ERROR) * expected:
+        pkm = km_pval(expected)
+        while pkm > risk_limit and expected < total_votes:
+            expected += 1
+            pkm = km_pval(expected)
+            if pkm == 1:
+                expected = total_votes
+                break
 
     return [margin, total_votes, initial_sample, expected]
